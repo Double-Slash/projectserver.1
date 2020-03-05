0b7b9c58b6d4dd6c61a6953e33a66443f927fcf3
songheeyeon <yeos60490@naver.com>
2020-03-05 10:12:06 +0900

    연송희 서버 과제

--- app.js
+++ app.js
@@ -3,38 +3,41 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 
-/////ROUTER/////////////////////////////////
+const bodyParser = require('body-parser');
+//var multer = require('multer');
+
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var infoRouter = require('./routes/info');
 var imageRouter = require('./routes/image');
 var videoRouter = require('./routes/video');
-///////////////////////////////////////////////
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/'));
 
-////////LINK//////////////////////////
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/info', infoRouter);
 app.use('/image', imageRouter);
 app.use('/video', videoRouter);
-/////////////////////////////////////
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,4 +55,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+
 module.exports = app;
+
--- routes/image.js
+++ routes/image.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  //res.render('image', { title: 'image' });
+  
+  res.send('<img src="public/images/image.png">');
+
+});
+
 module.exports = router;
